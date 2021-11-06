f19475a5478117c2249856ca5f00faf3d7acbaeb
alexanderknyazev <knyazev.alexander@gmail.com>
2021-11-06 13:34:06 +0500

    Initial Commit

---
+++ Kartoshka pro/Kartoshka_proApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kartoshka_proApp.swift
+//  Kartoshka pro
+//
+//  Created by Alexander Knyazev on 06.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Kartoshka_proApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
