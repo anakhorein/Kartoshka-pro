1985690063091de729f0a58491d71929764bc844
alexanderknyazev <knyazev.alexander@gmail.com>
2025-05-04 21:28:32 +0500

    all tests pass

---
+++ Kartoshka proTests/Kartoshka_proTests.swift
@@ -0,0 +1,16 @@
+//
+//  Kartoshka_proTests.swift
+//  Kartoshka proTests
+//
+//  Created by Alexander Knyazev on 04.05.2025.
+//
+
+import Testing
+
+struct Kartoshka_proTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
